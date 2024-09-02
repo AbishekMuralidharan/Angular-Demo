2e7646b55d6a7d73475f4014f4a8423c6ce363eb
AbishekMuralidharan <abishek.vmurali@outlook.com>
2024-09-02 13:38:34 +0530

    commit

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -35,6 +35,7 @@ export class LoginComponent {
         console.log("Github")
         console.log("bala");
         console.log("praba change the git")
+        console.log("Testing the GITHUB")
       }
     })
   }
